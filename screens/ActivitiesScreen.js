f1c13e417a6ab0bc4113db146eb5958d19d90fdb
Jose Fantasia <jose.fantasia@geneos.com.ar>
2019-01-31 16:13:36 -0300

    Apply header and footer refactoring in activities screen

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -7,6 +7,9 @@ import { StyleSheet, View, TouchableOpacity, Alert, ListView, ScrollView} from '
 
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
+import FooterNavBar from '../components/FooterNavBar';
+import HeaderNavBar from '../components/HeaderNavBar';
+
 export default class ActivitiesScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -104,24 +107,7 @@ export default class ActivitiesScreen extends React.Component {
 
     return (
       <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='yelp' style={{fontSize: 34, color: 'white'}}/>
-            </Button>
-          </Left>
-          <Body>
-            <Title>Actividades</Title>
-          </Body>
-          <Right>
-            <Button transparent onPress={() => this.props.navigation.navigate('Home')}  style={{fontSize: 30}}>
-              <Icon name='home'/>
-            </Button>
-            <Button transparent onPress={() => this.props.navigation.navigate('Map')}   style={{fontSize: 30}}>
-              <Icon name='map-marker'/>
-            </Button>
-          </Right>
-        </Header>
+        <HeaderNavBar title="Actividades" />
         <Content>
           
           <Card>
@@ -148,26 +134,8 @@ export default class ActivitiesScreen extends React.Component {
 
         </Content>
 
-        <Footer>
-          <FooterTab>
-            <Button vertical active onPress={() => this.props.navigation.navigate('Schedule')}>
-              <Icon name="tasks" />
-              <Text>Agenda</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="address-book" onPress={() => this.props.navigation.navigate('Contacts')}/>
-              <Text>Contactos</Text>
-            </Button>
-            <Button vertical>
-              <Icon active name="cog" />
-              <Text>Config</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="retweet" />
-              <Text>Sinc</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
+        <FooterNavBar navigation={this.props.navigation} />
+
       </Container>
     );
   }  
